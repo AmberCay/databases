4dbda917027e2b84670188aafb77b61aa7dc3155
Amber C <amberpomar@gmail.com>
2023-03-14 16:42:32 +0100

    dia1

--- database.js
+++ database.js
@@ -18,13 +18,98 @@ connection.connect((error) => {
 
 // add column
 
-let addC = "ALTER table direccion ADD COLUMN puerta INT;"
-connection.query(addC, (err, res) => {
+// let addC = "ALTER table direccion ADD COLUMN puerta INT;"
+// connection.query(addC, (err, res) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('added coloumn');
+//         console.log(res);
+//     }
+// });
+
+// drop column
+
+// let dropC = "ALTER table direccion DROP COLUMN puerta;"
+// connection.query(dropC, (err, res) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('Column deleted');
+//         console.log(res);
+//     }
+// });
+
+// drop table
+
+// let dropT = "DROP table direccion;"
+// connection.query(dropT, (err, res) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('Table deleted');
+//         console.log(res);
+//     }
+// });
+
+// set marks to 0
+
+let upD = "UPDATE marks SET mark = 0"
+connection.query(upD, (err, res) => {
     if(err) {
         console.log(err);
     }
     else {
-        console.log('added coloumn');
+        console.log('Set marks to 0');
+        console.log(res);
+    }
+});
+
+let getNL = "SELECT first_name, last_name FROM students;"
+connection.query(getNL, (err, res) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('Student\'s frist & last names');
+        console.log(res);
+    }
+});
+
+let getT = "SELECT * FROM students;"
+connection.query(getT, (err, res) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('Teacher data');
+        console.log(res);
+    }
+});
+
+// delete marks older than 10 years
+
+let delX = "DELETE FROM  marks WHERE date < 2013-03-14"
+connection.query(delX, (err, res) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('marks older than 10 years have been deleted');
+        console.log(res);
+    }
+});
+
+let pass = "UPDATE marks WHERE mark < 6 SET mark = 0;"
+connection.query(delX, (err, res) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('student marks uner 5 set to 5');
         console.log(res);
     }
 });
